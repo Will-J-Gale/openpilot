e1d36acf7eeedce1373d66af1639200196b7ac70
Will Gale <willgaleproductions@gmail.com>
2024-03-14 21:20:08 +0000

    WIP: check id

--- ajarpilot/sensord/lsm6ds3.py
+++ ajarpilot/sensord/lsm6ds3.py
@@ -2,9 +2,11 @@ from smbus2 import SMBus
 
 I2C_BUS = 1
 LSM6DS3_ADDR = 0x6A
-CHIP_ID_ADDR = 0xFA
+CHIP_ID_ADDR = 0x0F
+CHIP_ID = 0b01101010
 
 
 with SMBus(I2C_BUS) as bus:
-    b = bus.read_byte_data(LSM6DS3_ADDR, CHIP_ID_ADDR, 8)
-    print(f"{b:08b}")
+    read_id = bus.read_byte_data(LSM6DS3_ADDR, CHIP_ID_ADDR, 8)
+    print(f"{read_id:08b}")
+    print(read_id == CHIP_ID)
