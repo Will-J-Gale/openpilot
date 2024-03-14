a2a5eb352f4bf7230122e4a3b9cd309903c764c9
Will Gale <willgaleproductions@gmail.com>
2024-03-14 21:38:04 +0000

    WIP: accel reader

--- ajarpilot/sensord/lsm6ds3.py
+++ ajarpilot/sensord/lsm6ds3.py
@@ -5,8 +5,24 @@ LSM6DS3_ADDR = 0x6A
 CHIP_ID_ADDR = 0x0F
 CHIP_ID = 0b01101010
 
+#Accelerometer
+ACCEL_CTRL_ADDR = 0x10
+ACCEL_ENABLE_SETTINGS = 0b01000000 #104hz, +-2g, 
+ACCEL_DATA_ADDR = 0x28
+
+
 
 with SMBus(I2C_BUS) as bus:
-    read_id = bus.read_byte_data(LSM6DS3_ADDR, CHIP_ID_ADDR, 8)
+    read_id = bus.read_byte_data(LSM6DS3_ADDR, CHIP_ID_ADDR)
     print(f"{read_id:08b}")
     print(read_id == CHIP_ID)
+    
+    #Enable accelerometer
+    bus.write_byte_data(LSM6DS3_ADDR, ACCEL_CTRL_ADDR, ACCEL_ENABLE_SETTINGS)
+
+    while(True):
+        try:
+            accel_data = bus.read_i2c_block_data(LSM6DS3_ADDR, ACCEL_DATA_ADDR, 6)
+            print(accel_data)
+        except KeyboardInterrupt:
+            break
