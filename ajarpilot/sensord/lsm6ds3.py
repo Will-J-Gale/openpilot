429ffda2bacd23cfd4508ccd27cc13c9a6bb7641
Will Gale <willgaleproductions@gmail.com>
2024-03-14 21:41:00 +0000

    WIP: split into x,y,z

--- ajarpilot/sensord/lsm6ds3.py
+++ ajarpilot/sensord/lsm6ds3.py
@@ -1,5 +1,11 @@
 from smbus2 import SMBus
 
+def decode_bytes(lowByte, highByte):
+    value = (highByte<<8) + lowByte
+    if value >= (256*256)//2:
+        value = value - (256*256)
+    return value
+
 I2C_BUS = 1
 LSM6DS3_ADDR = 0x6A
 CHIP_ID_ADDR = 0x0F
@@ -23,6 +29,11 @@ with SMBus(I2C_BUS) as bus:
     while(True):
         try:
             accel_data = bus.read_i2c_block_data(LSM6DS3_ADDR, ACCEL_DATA_ADDR, 6)
-            print(accel_data)
+            x = decode_bytes(accel_data[0], accel_data[1])
+            y = decode_bytes(accel_data[2], accel_data[3])
+            z = decode_bytes(accel_data[4], accel_data[5])
+
+            print(x, y, z)
+
         except KeyboardInterrupt:
             break
