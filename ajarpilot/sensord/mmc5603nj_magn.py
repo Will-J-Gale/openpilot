2183fca483f14eb7b54b1ee26fc01adc991f1815
Will Gale <willgaleproductions@gmail.com>
2024-03-19 21:01:25 +0000

    WIP: Add magno script

---
+++ ajarpilot/sensord/mmc5603nj_magn.py
@@ -0,0 +1,14 @@
+from smbus2 import SMBus
+
+I2C_BUS = 1
+
+MMC5603NJ_I2C_ADDR = 0x30
+MMC5603NJ_PRODUCT_ID = 0b00010000
+
+def main():
+    with SMBus(I2C_BUS) as bus:
+        product_id = bus.read_byte_data(MMC5603NJ_I2C_ADDR, MMC5603NJ_PRODUCT_ID)
+        print(product_id, product_id==MMC5603NJ_PRODUCT_ID)
+
+if __name__ == "__main__":
+    main()
